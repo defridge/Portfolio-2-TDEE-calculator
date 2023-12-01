185b91c7202f4959ddf4f8a0b13994e083a6d9cb
defridge <>
2023-12-01 14:57:01 +0000

    Feat: Added js code to calculate bmr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,34 @@
+document.querySelector('.submit').addEventListener('click', function() {
 
-let age = document.getElementById("age").value;
-let height = document.getElementById("height").value;
-let weight = document.getElementById("weight").value;
+    let age = document.getElementById('age').value;
+    let height = document.getElementById('height').value;
+    let weight = document.getElementById('weight').value;
+    let genderSelect = document.getElementById('gender');
+    let gender = genderSelect.options[genderSelect.selectedIndex].value;
 
-
-function calculateBmr () {
-
-    let bmrM = (10 * weight) + (6.25 * height) - (5 * age) + 100;
-    let bmrF = (10 * weight) + (6.25 * height) - (5 * age) - 161;
-
-    if (document.getElementById("male")) {
-        document.getElementById("bmr").innerText = bmrM;
-    } else if (document.getElementById("female")) {
-        document.getElementById("bmr").innerText = bmrF;
+    if (gender === 'male') {
+        let bmrM = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        console.log(bmrM);
+        document.getElementById('bmr').innerText = bmrM;
+    } else if (gender === 'female') {
+        let bmrF = (10 * weight) + (6.26 * height) - (5 * age) - 161;
+        console.log(bmrF);
+        document.getElementById('bmr').innerText = bmrF;
     }
-}
 
-calculateBmr();
+    /*if (document.getElementById('male')) {
+        let bmrM = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        console.log(bmrM);
+        document.getElementById('bmr').innerText = bmrM;
+    } else if (document.getElementById('female')) {
+        let bmrF = (10 * weight) + (6.26 * height) - (5 * age) - 161;
+        console.log(bmrF);
+        document.getElementById('bmr').innerText = bmrF;
+    }*/
+    
+});
+
+/*let bmrF = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+
+console.log(bmrM);
+console.log(bmrF); */
