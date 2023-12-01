d30530a22d7feb1befb8a6bbf9ec0e5832c76316
defridge <>
2023-12-01 17:06:25 +0000

    Feat: Finished code to calculate tdee for males and females

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,31 @@ document.querySelector('.submit2').addEventListener('click', function() {
         let bmr = document.getElementById('bmr').innerText;
         let sedentary = bmr * 0.2;
         let sedentaryTdee = parseFloat(bmr) + parseFloat(sedentary);
-        document.getElementById('tdee').innerText = sedentaryTdee;
+        document.getElementById('tdee').innerText = Math.round(sedentaryTdee);
         console.log(sedentaryTdee);
-    } 
-})
+    } else if (activityOption === 'light-active') {
+        let bmr = document.getElementById('bmr').innerText;
+        let light = bmr * 0.37;
+        let lightTdee = parseFloat(bmr) + parseFloat(light);
+        document.getElementById('tdee').innerText = Math.round(lightTdee);
+        console.log(lightTdee);
+    } else if (activityOption === 'moderate-active') {
+        let bmr = document.getElementById('bmr').innerText;
+        let moderate = bmr * 0.55;
+        let moderateTdee = parseFloat(bmr) + parseFloat(moderate);
+        document.getElementById('tdee').innerText = Math.round(moderateTdee);
+        console.log(moderateTdee);
+    } else if (activityOption === 'active') {
+        let bmr = document.getElementById('bmr').innerText;
+        let active = bmr * 0.72;
+        let activeTdee = parseFloat(bmr) + parseFloat(active);
+        document.getElementById('tdee').innerText = Math.round(activeTdee);
+        console.log(activeTdee);
+    } else if (activityOption === 'high-active') {
+        let bmr = document.getElementById('bmr').innerText;
+        let highActive = bmr * 0.9;
+        let highActiveTdee = parseFloat(bmr) + parseFloat(highActive);
+        document.getElementById('tdee').innerText = Math.round(highActiveTdee);
+        console.log(highActiveTdee);
+    }
+});
