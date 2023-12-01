bde2003d37742dee647d80bde1199650247d4105
defridge <>
2023-12-01 16:47:59 +0000

    Feat: Added js code for tdee section

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,21 +14,19 @@ document.querySelector('.submit').addEventListener('click', function() {
         let bmrF = (10 * weight) + (6.26 * height) - (5 * age) - 161;
         console.log(bmrF);
         document.getElementById('bmr').innerText = bmrF;
-    }
-
-    /*if (document.getElementById('male')) {
-        let bmrM = (10 * weight) + (6.25 * height) - (5 * age) + 5;
-        console.log(bmrM);
-        document.getElementById('bmr').innerText = bmrM;
-    } else if (document.getElementById('female')) {
-        let bmrF = (10 * weight) + (6.26 * height) - (5 * age) - 161;
-        console.log(bmrF);
-        document.getElementById('bmr').innerText = bmrF;
-    }*/
-    
+    } 
 });
 
-/*let bmrF = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+document.querySelector('.submit2').addEventListener('click', function() {
 
-console.log(bmrM);
-console.log(bmrF); */
+    let activityMulti = document.getElementById('activity-multiplier');
+    let activityOption = activityMulti.options[activityMulti.selectedIndex].value;
+
+    if (activityOption === 'sedentary') {
+        let bmr = document.getElementById('bmr').innerText;
+        let sedentary = bmr * 0.2;
+        let sedentaryTdee = parseFloat(bmr) + parseFloat(sedentary);
+        document.getElementById('tdee').innerText = sedentaryTdee;
+        console.log(sedentaryTdee);
+    } 
+})
