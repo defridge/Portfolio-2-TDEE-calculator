735cbd891f3b852399ef674ce0b01aa843c5fac0
defridge <>
2023-12-07 15:24:46 +0000

    Feat: Added new js file to apply dry coding principals

---
+++ assets/js/script1.js
@@ -0,0 +1,18 @@
+document.querySelector('.submit').addEventListener('click', calcBmr);
+
+function calcBmr() {
+    let age = document.getElementById('age').value;
+    let height = document.getElementById('height').value;
+    let weight = document.getElementById('weight').value;
+    let genderSelect = document.getElementById('gender');
+    let gender = genderSelect.options[genderSelect.selectedIndex].value;
+
+    if (gender === 'male') {
+        let bmrM = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        document.getElementById('bmr').innerText = bmrM;
+    } else if (gender === 'female') {
+        let bmrF = (10 * weight) + (6.26 * height) - (5 * age) - 161;
+        document.getElementById('bmr').innerText = bmrF;
+    }
+}
+
