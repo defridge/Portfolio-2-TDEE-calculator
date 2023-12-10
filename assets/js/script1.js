da9666761030611a05232b80afbcc2a99c305bbe
defridge <>
2023-12-10 12:45:54 +0000

    Feat: Added code and styles for a modal box

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -63,3 +63,18 @@ function calcNewKcals() {
             break;
     }
 };
+
+let modal = document.getElementById('macros');
+let btn = document.getElementsByClassName('submit4')[0];
+let span = document.getElementsByClassName('close')[0];
+btn.onclick = function() {
+    modal.style.display = 'block';
+};
+span.onclick = function() {
+    modal.style.display = 'none';
+};
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
